4f1dd8790217dfaea0e16bc1d1fbf423c8fd31c3
Greg Albrecht <gba@splunk.com>
2012-03-21 15:01:26 -0700

    Bumping version to 0.2.1 and adding __version__ and __version_info__ to module.

--- chef/__init__.py
+++ chef/__init__.py
@@ -1,4 +1,8 @@
 # Copyright (c) 2010 Noah Kantrowitz <noah@coderanger.net>
+
+__version_info__ = ('0', '2', '1')
+__version__ = '.'.join(__version_info__)
+
 from chef.api import ChefAPI, autoconfigure
 from chef.client import Client
 from chef.data_bag import DataBag, DataBagItem
